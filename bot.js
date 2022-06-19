33667be09052ebae2dc075a62f15120d3c545031
VodkaX <100626938+vodkax0@users.noreply.github.com>
2022-06-19 14:05:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
 const request = require('request')
 const config = require("./config.json")
 
-// selam ben aref
+// selam ben vodka
     client.on("ready", () => {
         client.user.setPresence({ activity: { name: config.durum }, status: "idle" });
         client.channels.cache.get(config.voicekanal).join().catch(x => x)
@@ -12,34 +12,34 @@ const config = require("./config.json")
 
 
     setInterval(async () => {
-        if(guild.vanityURLCode == config.vanity) {// aref
+        if(guild.vanityURLCode == config.vanity) {// vodka
             let onay = new Discord.MessageEmbed().setAuthor(guild.name, guild.iconURL({dynamic: true})).setColor('RANDOM').setTimestamp().setDescription(`"**${guild.name}**" sunucusunun özel url'si belirtildiği gibi **\`${config.vanity}\`** olarak değiştirildi :white_check_mark:`)
-                let kanal = await guild.channels.cache.get(config.urllog)// aref
+                let kanal = await guild.channels.cache.get(config.urllog)// vodka
                      await kanal.send(onay)
-                        await kanal.send(`<@${config.botOwner}>`)// aref
+                        await kanal.send(`<@${config.botOwner}>`)// vodka
 
 
     console.log(`${guild.name} = "${guild.vanityURLCode}" Durum: ✔`) 
-        console.log(`Bottan Çıkış Yapıldı.`)// aref
+        console.log(`Bottan Çıkış Yapıldı.`)//vodka
             process.exit()
-                } else {// aref
+                } else {// vodka
                     arefyyy(config.vanity, config.guild, config.token)
-                         }}, 1*500)})// aref
+                         }}, 1*500)})// vodka
 
     async function arefyyy(vanity, token) {
-        const spammer = {//aref
+        const spammer = {//vodka
             url: `https://discord.com/api/v8/guilds/${config.sunucu}/vanity-url`,
                 body: {
                     code: `${vanity}`},
     json: true,
         method: 'PATCH',
-            headers: {//aref
+            headers: {//vodka
                 "Authorization": `Bot ${config.token}`
                     }
                         };
 
     request(spammer, (err, res, body) => {
-        if (err) {//aref
+        if (err) {//vodka
             return console.log(err)}})}
 
 
